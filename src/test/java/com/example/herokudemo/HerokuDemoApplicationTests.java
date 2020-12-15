04975112580a98a7f6d6d001d17c58e2291916d9
chameekara <chameekara.karandana@gmail.com>
2020-12-15 13:42:37 +0530

    initial commit

---
+++ src/test/java/com/example/herokudemo/HerokuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokudemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
